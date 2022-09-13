7d1f0a9a6fb212f182f6baca16d78fad93fdcdfa
ChiendeGlace <szymon.szostak72@gmail.com>
2022-09-13 17:01:33 +0200

    Fix range slider

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const body = document.querySelector('body');
 const wrapper = document.querySelector('.wrapper');
-const submit = document.querySelector('#submit');
-submit.addEventListener('click', makeGrid); 
+const input = document.querySelector('input');
+const squares = document.querySelectorAll('.square');
+const rangeValue = document.querySelector('#range-value');
+const black = document.querySelector('#black');
+const rainbow = document.querySelector('#rainbow');
+const eraser = document.querySelector('#eraser');
+const clear = document.querySelector('#clear');
+
+input.addEventListener('change', makeGrid);
+
 function makeGrid(e) {
-  let n = e.target.value;
-  console.log('Hello world');
+  const squares = document.querySelectorAll('.square');
+  for (let i = 0; i < squares.length; i++) {
+    wrapper.removeChild(squares[i]);
+  }
+  let n = input.value;
   for (let i = 0; i < n * n; i++) {
     let square = document.createElement('div');
     wrapper.appendChild(square);
@@ -16,7 +27,14 @@ function makeGrid(e) {
   }
   wrapper.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
   wrapper.style.gridTemplateRows = `repeat(${n}, 1fr)`;
+  if (input.value !== 0) {
+    rangeValue.textContent = `Grid size: ${input.value} X ${input.value}`;
+  };
 };
+
+
+
+
 
 
 
