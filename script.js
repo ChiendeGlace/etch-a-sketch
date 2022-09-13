53f0e641183ef559c11986d87504bb809a9c0b9f
ChiendeGlace <szymon.szostak72@gmail.com>
2022-09-13 17:36:23 +0200

    Fix bugs and add functionality to buttons

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 const body = document.querySelector('body');
 const wrapper = document.querySelector('.wrapper');
 const input = document.querySelector('input');
-const squares = document.querySelectorAll('.square');
 const rangeValue = document.querySelector('#range-value');
 const black = document.querySelector('#black');
 const rainbow = document.querySelector('#rainbow');
 const eraser = document.querySelector('#eraser');
 const clear = document.querySelector('#clear');
-
 input.addEventListener('change', makeGrid);
+black.addEventListener('click', changeColorBlack);
+eraser.addEventListener('click', changeColorWhite);
+rainbow.addEventListener('click', changeColor);
+clear.addEventListener('click', clearGrid);
+
+function changeColor(e) {
+  wrapper.childNodes.forEach(div => div.addEventListener('mouseover', randomColor));
+  function randomColor(e) {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  }
+};
+
+function changeColorBlack(e) {
+  wrapper.childNodes.forEach(div => div.addEventListener('mouseover', randomColor));
+  function randomColor(e) {
+    this.style.backgroundColor = `black`;
+  }
+};
+
+function changeColorWhite(e) {
+  wrapper.childNodes.forEach(div => div.addEventListener('mouseover', randomColor));
+  function randomColor(e) {
+    this.style.backgroundColor = 'white';
+  }
+};
+
+function clearGrid(e) {
+  wrapper.childNodes.forEach(div => div.style.backgroundColor = 'white');
+};
 
 function makeGrid(e) {
   const squares = document.querySelectorAll('.square');
@@ -24,14 +51,13 @@ function makeGrid(e) {
     function giveColor(e) {
       square.style.backgroundColor = 'black';
     };
-  }
+  };
   wrapper.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
   wrapper.style.gridTemplateRows = `repeat(${n}, 1fr)`;
   if (input.value !== 0) {
     rangeValue.textContent = `Grid size: ${input.value} X ${input.value}`;
   };
 };
-
 
 
 
